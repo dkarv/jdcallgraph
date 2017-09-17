f5204ffa3264c86d410d7489d7f74b2feebdde11
David Krebs <d.krebs@testbirds.de>
2017-09-17 08:31:53 +0200

    add test for finish

--- jdcallgraph/src/test/java/com/dkarv/jdcallgraph/callgraph/CallGraphTest.java
+++ jdcallgraph/src/test/java/com/dkarv/jdcallgraph/callgraph/CallGraphTest.java
@@ -1,6 +1,8 @@
 package com.dkarv.jdcallgraph.callgraph;
 
 import com.dkarv.jdcallgraph.callgraph.writer.DotFileWriter;
+import com.dkarv.jdcallgraph.callgraph.writer.GraphWriter;
+import com.dkarv.jdcallgraph.util.StackItem;
 import com.dkarv.jdcallgraph.util.Target;
 import com.dkarv.jdcallgraph.util.config.Config;
 import com.dkarv.jdcallgraph.util.config.ConfigUtils;
@@ -8,6 +10,8 @@ import org.hamcrest.CoreMatchers;
 import org.junit.Assert;
 import org.junit.Test;
 import org.mockito.Mockito;
+
+import java.io.IOException;
 
 public class CallGraphTest {
 
@@ -20,5 +24,20 @@ public class CallGraphTest {
     Mockito.when(c.writeTo()).thenReturn(Target.DOT);
     graph.createWriter();
     Assert.assertThat(graph.writer, CoreMatchers.instanceOf(DotFileWriter.class));
+  }
+
+  @Test
+  public void testFinish() throws IOException {
+    CallGraph graph = new CallGraph(1);
+    GraphWriter writer = Mockito.mock(GraphWriter.class);
+    StackItem item = Mockito.mock(StackItem.class);
+    graph.writer = writer;
+
+
+    graph.finish();
+    Mockito.verifyNoMoreInteractions(writer);
+    graph.calls.push(item);
+    graph.finish();
+    Mockito.verify(writer).end();
   }
 }
