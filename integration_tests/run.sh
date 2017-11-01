41025eadfcc48374eea5c2aa1183398eeb641e6b
David Krebs <davkrebs@gmail.com>
2017-11-01 17:19:36 +0100

    exit on compile error

--- integration_tests/run.sh
+++ integration_tests/run.sh
@@ -4,15 +4,22 @@ rm -r result
 mkdir -p target
 mkdir -p result
 
-find src/com/dkarv/verifier -name "*.java" -print | xargs javac -d target
-find src/com/dkarv/testcases/$1 -name "*.java" -print | xargs javac -cp target -d target
+
+find src/com/dkarv/verifier -name "*.java" -print | \
+xargs javac -d target || exit 255
+
+find src/com/dkarv/testcases/$1 -name "*.java" -print | \
+xargs javac -cp target -d target || exit 255
+
 
 java -cp target \
 -javaagent:../jdcallgraph/target/jdcallgraph-0.2-agent.jar=./config.ini \
 com/dkarv/testcases/$1/Main
 
+
 if java -cp target com/dkarv/testcases/$1/Verification ; then
     echo "Verification of >>  $1  << succeeded" >&2
 else
     echo "Verification of >>  $1  << failed" >&2
+    exit 1
 fi
