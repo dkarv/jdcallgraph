62cdf9456c0e45ce7fb4066ea1603d39b6d376ea
David Krebs <davkrebs@gmail.com>
2017-11-01 18:31:37 +0100

    run tests on javassist and bytebuddy

--- integration_tests/run.sh
+++ integration_tests/run.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 rm -r result
+rm -r result2
 mkdir -p target
 mkdir -p result
 
-
+# Compile
 find src/com/dkarv/verifier -name "*.java" -print | \
 xargs javac -d target || exit 255
 
@@ -12,17 +13,23 @@ find src/com/dkarv/testcases/$1 -name "*.java" -print | \
 xargs javac -cp target -d target || exit 255
 
 
-java -cp target \
--javaagent:../jdcallgraph/target/jdcallgraph-0.2-agent.jar=./bytebuddy.ini \
-com/dkarv/testcases/$1/Main
-java -cp target \
--javaagent:../jdcallgraph/target/jdcallgraph-0.2-agent.jar=./javassist.ini \
-com/dkarv/testcases/$1/Main
+function run {
+    echo "## Run tests with $2" >&2
+    java -cp target \
+    -javaagent:../jdcallgraph/target/jdcallgraph-0.2-agent.jar=./$2 \
+    com/dkarv/testcases/$1/Main
+    if java -cp target com/dkarv/testcases/$1/Verification ; then
+        echo "Verification of >>  $1  << succeeded" >&2
+    else
+        echo "Verification of >>  $1  << failed" >&2
+        exit 1
+    fi
+}
 
+run $1 "bytebuddy.ini"
+mv result result2
+mkdir result
 
-if java -cp target com/dkarv/testcases/$1/Verification ; then
-    echo "Verification of >>  $1  << succeeded" >&2
-else
-    echo "Verification of >>  $1  << failed" >&2
-    exit 1
-fi
+run $1 "javassist.ini"
+diff -r result/cg result2/cg
+diff -r result/ddg result2/ddg
