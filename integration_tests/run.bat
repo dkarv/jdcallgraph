0ba95efe1d13cb05c3584a9c50b9ae5c4c08b85a
David Krebs <davkrebs@gmail.com>
2017-11-07 00:23:53 +0100

    more fixes for integration tests on windows

--- integration_tests/run.bat
+++ integration_tests/run.bat
@@ -14,3 +14,30 @@ for /F %%G in (src\com\dkarv\verifier\*.java) do (
 for /F %%G in (src\com\dkarv\testcases\%1\*.java) do (
     javac -d target %%G
 )
+
+:run
+echo Run %1 %2
+java -cp target ^
+-javaagent:..\jdcallgraph\target\jdcallgraph-0.2-agent.jar=.\%2 ^
+com.dkarv.testcases.%1.Main
+
+::    if java -cp target com/dkarv/testcases/$1/Verification ; then
+::        echo "Verification of >>  $1 with $2  << succeeded" >&2
+::    else
+::        echo "Verification of >>  $1 with $2  << failed" >&2
+::        exit 1
+::    fi
+
+:: run $1 "bytebuddy.ini"
+
+::if [[ "$version" < "1.9" ]]; then
+::    mv result result2
+::    mkdir result
+
+::    run $1 "javassist.ini"
+
+::    if [[ -f "src/com/dkarv/testcases/$1/.nodiff" ]]; then
+::        diff -r -X src/com/dkarv/testcases/$1/.nodiff result/cg result2/cg >&2
+::    else
+::        diff -r result/cg result2/cg >&2
+::    fi
